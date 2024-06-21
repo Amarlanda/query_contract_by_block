07aa7652c0dc766a2aeda16298d33382d1d6a4c7
Amar Landa <AL@amarlanda.com>
2024-06-21 11:23:59 +0100

    sanity check - .env file exist before run

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@
 
 reset ; clear 
 
+# Function to check if .env file exists
+function check_env_file() {
+    if [ ! -f .env ]; then
+        echo ".env file does not exist!"
+        exit 1
+    fi
+    echo ".env file exists."
+}
+
+check_env_file
+
 cargo run
